7ae8a4c10dd56439ae525c7aae07a6cbc5f83a7a
Jenny Lin <misslinyang91@gmail.com>
2021-08-19 18:44:31 +1000

    added size and shape function

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 let blockEl = document.querySelector("#block");
 let verticalPosotion = document.querySelector("#position-y");
 let horizontalPosotion = document.querySelector("#position-x");
+let blockSize = document.querySelector("#size");
+let shapeSelector = document.querySelector("#shape-select");
+let okBtn = document.querySelector("#ok-btn");
 
 //vertical posotion changer
 verticalPosotion.addEventListener("change", function () {
@@ -10,4 +13,19 @@ verticalPosotion.addEventListener("change", function () {
 //horizontal posotion changer
 horizontalPosotion.addEventListener("change", function () {
   blockEl.style.left = horizontalPosotion.value + "px";
+});
+
+//size changer
+blockSize.addEventListener("change", function () {
+  blockEl.style.transform = "scale(" + blockSize.value + ")";
+});
+
+//shape changer
+okBtn.addEventListener("click", function () {
+  let shapeOption = shapeSelector.value;
+  if (shapeOption === "1") {
+    blockEl.style.borderRadius = "0";
+  } else if (shapeOption === "2") {
+    blockEl.style.borderRadius = "50%";
+  }
 });
