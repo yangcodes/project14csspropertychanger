4635d01fd3a3e83cab104d0ba30757fd8c21b9aa
Jenny Lin <misslinyang91@gmail.com>
2021-08-19 18:59:00 +1000

    added background color rgba change function

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ let horizontalPosotion = document.querySelector("#position-x");
 let blockSize = document.querySelector("#size");
 let shapeSelector = document.querySelector("#shape-select");
 let okBtn = document.querySelector("#ok-btn");
+let rgbaR = document.querySelector("#rgba-r");
+let rgbaG = document.querySelector("#rgba-g");
+let rgbaB = document.querySelector("#rgba-b");
+let rgbaA = document.querySelector("#rgba-a");
+let rgbaContainer = document.querySelector(".rgba-container");
+let rgbaInputs = rgbaContainer.querySelectorAll("input");
 
 //vertical posotion changer
 verticalPosotion.addEventListener("change", function () {
@@ -29,3 +35,19 @@ okBtn.addEventListener("click", function () {
     blockEl.style.borderRadius = "50%";
   }
 });
+
+//background color changer
+for (let i = 0; i < rgbaInputs.length; i++) {
+  rgbaInputs[i].addEventListener("change", function () {
+    blockEl.style.backgroundColor =
+      "rgba(" +
+      rgbaR.value +
+      "," +
+      rgbaG.value +
+      "," +
+      rgbaB.value +
+      "," +
+      rgbaA.value +
+      ")";
+  });
+}
